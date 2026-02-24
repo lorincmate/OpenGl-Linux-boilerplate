d2d0cf75b98a7275abcde39056addef20cb61a15
Máté Lőrinc <lorincmate@gmail.com>
2026-02-24 01:01:16 +0100

    Use custom shader instead of GPU manufacturare provided default one

--- src/application.cpp
+++ src/application.cpp
@@ -2,6 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string &source)
+{
+    unsigned int id = glad_glCreateShader(type);
+    const char* src = source.c_str();
+    glad_glShaderSource(id, 1, &src, nullptr);
+    glad_glCompileShader(id);
+
+    int result;
+    glad_glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glad_glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glad_glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glad_glDeleteShader(id);
+        return 0;
+    } 
+    return id;
+}
+
+static int CreateShader(const std::string &vertexShader, const std::string &fragmentShader)
+{
+    unsigned int program = glad_glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glad_glAttachShader(program, vs);
+    glad_glAttachShader(program, fs);
+    glad_glLinkProgram(program);
+    glad_glValidateProgram(program);
+
+    glad_glDeleteShader(vs);
+    glad_glDeleteShader(fs);
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -30,6 +69,8 @@ int main(void)
     unsigned int VBO;
     glad_glGenBuffers(1, &VBO);
     glad_glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glad_glEnableVertexAttribArray(0);
+    glad_glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
     float vertices[] = {
@@ -39,17 +80,29 @@ int main(void)
     };
     glad_glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    unsigned int shader = CreateShader(
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n",
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n"
+    );
+    glad_glUseProgram(shader);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableVertexAttribArray(0);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
